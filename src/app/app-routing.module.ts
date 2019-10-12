1c74118c7678e8aac8c64fd562a53ae3c7576a06
collozy <ozlocollins@gmail.com>
2019-10-12 12:38:25 +0300

    Added the first pages but yet to write functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
+  { path: 'topics', loadChildren: './pages/topics/topics.module#TopicsPageModule' },
+  { path: 'questions', loadChildren: './pages/questions/questions.module#QuestionsPageModule' },
 ];
 
 @NgModule({
